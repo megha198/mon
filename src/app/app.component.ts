ad0c3fcac2b7edfad5476ab62f07d0dc6dd198d9
Megha S.r <m1064528@mindtree.com>
2021-06-14 21:14:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log(this.square(6));
+    
+  }
   title = 'lintingdemo';
+square(x:any){
+  return x*x;
+  }
 
-sayHello() {
-  let message='Hello';
-  message=message + 'Pragim';
-  console.log(message);
-}
 
 }
 
